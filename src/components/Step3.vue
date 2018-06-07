08d2414a7a7edefe0392e97b82bb8aed430f6ebc
vuong.minh.thai <FRAMGIA\vuong.minh.thai@framgia0347-pc.framgia.com>
2018-06-07 16:55:29 +0700

    first_day

---
+++ src/components/Step3.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <h2>This is the final page</h2>
+        <p>Say something before you go</p>
+        <input type="text" v-model="isaid">
+        <br>
+        <p>You said: <span>{{ isaid }}</span></p>
+        <router-link to='/'>Home</router-link>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Step3',
+    data() {
+        return {
+            isaid: ''
+        }
+    }
+}
+</script>
+
+<style lang="postcss" scoped>
+    span {
+        color: red;
+    }
+</style>
