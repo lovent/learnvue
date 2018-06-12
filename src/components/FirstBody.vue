c90a54805bb1406a6433604afe99d5cc2339b835
lovent <vuong.minh.thai@framgia.com>
2018-06-12 08:57:48 +0700

    second_day

--- src/components/FirstBody.vue
+++ src/components/FirstBody.vue
@@ -2,10 +2,10 @@
     <div class="body">
         <section class="first-section">
             <h3>Your information</h3>
-            <p>Your name: <input type="text" placeholder="name"></p>
-            <p>Your email: <input type="email" placeholder="email"></p>
+            <p>Your name: <input v-model="user.name" type="text" placeholder="name"></p>
+            <p>Your password: <input v-model="user.password" type="password" placeholder="password"></p>
             <p>Your gender:
-                <select name="gender" id="">
+                <select v-model="user.gender" name="gender" id="">
                     <option value="1" selected disabled>---Select your gender---</option>
                     <option value="2">Male</option>
                     <option value="3">Female</option>
@@ -14,6 +14,10 @@
             </p>
             <button>Confirm</button>
             <button>Cancel</button>
+            <h1 v-if="user.name == null && user.gender == null" style="color: orange">Welcome to my app</h1>
+            <h1 v-else-if="user.gender == 2 && user.name != null" style="color: green">Hello Sir, {{user.name}}</h1>
+            <h1 v-else-if="user.gender == 3 && user.name != null" style="color: red">Hello Madam, {{user.name}}</h1>
+            <h1 v-else style="color: black">Hello</h1>
             <br>
             <br>
             <router-link to='/Step2'>Next Step</router-link>
@@ -26,7 +30,7 @@ export default {
     name: 'body',
     data () {
         return {
-            msg: ''
+            user: {}
         }
     }
 }
--- src/components/Step2.vue
+++ src/components/Step2.vue
@@ -1,16 +1,44 @@
 <template>
     <section class="second-section">
-      <img src="../assets/yoona-2.jpg" alt="mylove">
+        <div class="row">
+            <div class="nation" v-for="nation of nations" :key="nation">
+                <h4>{{ nation.name }}</h4>
+                <img :src="nation.flag">
+            </div>
+        </div>
         <div>
             <router-link to='/'>Previous</router-link>
             <router-link to='/step3'>Next</router-link>
         </div>
     </section>
-
 </template>
+
+<script>
+export default {
+    name: 'second-section',
+    data () {
+        return {
+            nations: [
+                {name: 'Argentina', flag: 'http://flags.fmcdn.net/data/flags/w580/ar.png'},
+                {name: 'Brazil', flag: 'http://flags.fmcdn.net/data/flags/w580/br.png'},
+                {name: 'Denmark', flag: 'http://flags.fmcdn.net/data/flags/w580/dk.png'},
+                {name: 'England', flag: 'http://flags.fmcdn.net/data/flags/w580/gb.png'},
+                {name: 'France', flag: 'http://flags.fmcdn.net/data/flags/w580/fr.png'},
+                {name: 'Germany', flag: 'http://flags.fmcdn.net/data/flags/w580/de.png'},
+                {name: 'Holland', flag: 'http://flags.fmcdn.net/data/flags/w580/nl.png'},
+                {name: 'Italy', flag: 'http://flags.fmcdn.net/data/flags/w580/it.png'},
+                {name: 'Japan', flag: 'http://flags.fmcdn.net/data/flags/w580/jp.png'},
+                {name: 'Korea', flag: 'http://flags.fmcdn.net/data/flags/w580/kr.png'},
+                {name: 'Nigeria', flag: 'http://flags.fmcdn.net/data/flags/w580/ng.png'},
+                {name: 'Vietnam', flag: 'http://flags.fmcdn.net/data/flags/w580/vn.png'}
+            ]
+        }
+    }
+}
+</script>
 
 <style scoped>
     img {
-        width: 800px;
+        width: 400px;
     }
 </style>
